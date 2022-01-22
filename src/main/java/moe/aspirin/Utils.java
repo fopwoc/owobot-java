74e8f9e29ba6ce0a6aca0df93c0db66675d7f007
Ilya Dobryakov <aspirin@govno.tech>
2022-01-22 13:28:40 +0300

    broken subreddit messege

---
+++ src/main/java/moe/aspirin/Utils.java
@@ -0,0 +1,14 @@
+package moe.aspirin;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class Utils {
+    final private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd ");
+
+    public String timeAndDate() {
+        return ("[" + simpleDateFormat.format(new Date()) + DateTimeFormatter.ofPattern("HH:mm:ss").format(LocalTime.now()) + "] ");
+    }
+}
